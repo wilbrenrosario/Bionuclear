1624ce1bfc256a385cc75601426b271c3ebd206f
wrosario <wrosario@unit.com.do>
2023-09-19 10:56:08 -0400

    resultados ingresados

--- backend/Bionuculear-Backend/Bionuclear.Infrastructure/Sql/Commands/Resultados/ResultadosCommandHandler.cs
+++ backend/Bionuculear-Backend/Bionuclear.Infrastructure/Sql/Commands/Resultados/ResultadosCommandHandler.cs
@@ -16,6 +16,26 @@ namespace Bionuclear.Infrastructure.Sql.Commands.Resultado
         }
         public async Task<ResultadosDtos> Handle(ResultadosCommand request, CancellationToken cancellationToken)
         {
+            var usuario = _context.Usuarios.Where(x => x.correo_electronico == request.Resultados.correo_electroncio_paciente).FirstOrDefault();
+            
+            if (usuario == null)
+            {
+                var new_user = new Usuarios
+                {
+                    usuario = "user" + Guid.NewGuid().ToString(),
+                    clave = "123456",
+                    nombre_completo = request.Resultados.nombre_paciente,
+                    correo_electronico = request.Resultados.correo_electroncio_paciente,
+                    tipo_usuario = 1 //Usuario cliente
+                };
+                await _context.Usuarios.AddAsync(new_user);
+                await _context.ColaCorreos.AddAsync(new ColaCorreos { correo_electronico = request.Resultados.correo_electroncio_paciente });
+            }
+            else
+            {
+                await _context.ColaCorreos.AddAsync(new ColaCorreos { correo_electronico = request.Resultados.correo_electroncio_paciente });
+            }
+
             var result = new Bionuclear.Core.Models.Resultados.Resultados
             {
                 comentario = request.Resultados.comentario,
@@ -28,6 +48,7 @@ namespace Bionuclear.Infrastructure.Sql.Commands.Resultado
             };
 
             await _context.Resultados.AddAsync(result);
+
             await _context.SaveChangesAsync();
 
             return request.Resultados;
