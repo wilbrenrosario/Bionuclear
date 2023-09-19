1f83fa889d611a577e4c02cfaee488db699d5e78
wrosario <wrosario@unit.com.do>
2023-09-19 05:54:12 -0400

    fix

---
+++ backend/Bionuculear-Backend/Bionuclear.Api/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Bionuclear.Core.Dtos;
+using Bionuclear.Infrastructure.Sql.Commands.Login;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bionuclear.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Login() { 
+
+            return Ok("HOLA");
+        }
+    }
+}
