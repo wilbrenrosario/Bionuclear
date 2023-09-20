c27fba15d5e05e709b2da6d520d797f4ed35b98e
wrosario <wrosario@unit.com.do>
2023-09-20 01:00:23 -0400

    .

--- backend/Bionuculear-Backend/Bionuclear.Api/Controllers/FilesController.cs
+++ backend/Bionuculear-Backend/Bionuclear.Api/Controllers/FilesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Bionuclear.Api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class FilesController : ControllerBase
